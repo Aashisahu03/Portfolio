19edd50633804ce979314f8f92eb0aaf32e4e7ab
Aashi sahu <aashisahu0302@gmail.com>
2026-05-11 20:17:03 +0530

    resume update

--- src/lib/components/Contacts.tsx
+++ src/lib/components/Contacts.tsx
@@ -277,7 +277,7 @@ export default function Contact() {
                             <h3 style={{ fontSize: "1.5rem", fontWeight: 700 }}>Resume</h3>
                             <p>Download my latest resume to see my experience and projects.</p>
                             <a
-                                href="https://drive.google.com/file/d/1jn10b6gQBPQD4Xbo-yV4Lig4G56U8wo_/view?usp=sharing"
+                                href="https://drive.google.com/file/d/1n_bESO7uCY15KwVvchHtCDl4vX3VCZwu/view?usp=sharing"
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 style={{ textDecoration: "none" }}
